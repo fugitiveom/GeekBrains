cd859d2267825955aacb715f2af9dfece0045d3e
fugitive <fugitiveom@gmail.com>
2023-06-15 23:51:39 +0400

    v.1

---
+++ 2023-06-15_homework.py
@@ -0,0 +1,15 @@
+def arith(arr):
+    return sum(arr)/len(arr)
+
+
+def factorial(n):
+    if n <= 0 and n % 2 == 0:
+        return "Факториал может быть расчитан только из целого положительного числа"
+    else:
+        result = 1
+        for i in range(1,n+1):
+            result *= i
+        return result
+
+print(arith([1,2,6,45,8,5,3]))
+print(factorial(10))
