147b139c7b2d3e2413a67bb2a61d7b76396e5bdd
fugitive <fugitiveom@gmail.com>
2023-06-22 00:30:32 +0400

    init

--- 2023-20-06/task_3.py
+++ 2023-20-06/task_3.py
@@ -1,15 +1,21 @@
 def sum_min_to_max(arr) -> float:
+    arr.sort()
     return sum(arr[1: -1])
 
 
 def sum_min_to_max2(arr) -> float:
+    min_num = arr[0]
+    max_num = arr[0]
     i = 0
-    result = 0
     while i < len(arr):
-        if i > 0 and i < len(arr) -1:
-            result += arr[i]
-        i += 1
-    return result
+        if arr[i] > max_num:
+            max_num = num
+        elif arr[i] < min_num:
+            min_num = num
+    arr.remove(max_num)
+    arr.remove(min_num)
+    return sum(arr)
+
 
 
 print(sum_min_to_max([1,5,6,4,8,87,5,7]))
